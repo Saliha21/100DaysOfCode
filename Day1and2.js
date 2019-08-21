03eeeb29200ce08999a119d817f2143cd3dab44e
Saliha Almalki <44327944+Saliha21@users.noreply.github.com>
2019-08-21 22:20:15 +0300

    Add files via upload

---
+++ Day1and2.js
@@ -0,0 +1,12 @@
+
+// The first and second day lesson - print and comments 
+// Saliha Almalki
+
+document.write(" Hello world, welcome to java script "); // print Hello world, welcome to java script
+document.write(5+6); // print 11 
+
+// single line comment
+
+
+/* Multi-line 
+   comment */
