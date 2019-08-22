7ace1c07c91cddc883d4159dc34473fe9ab80923
Saliha Almalki <44327944+Saliha21@users.noreply.github.com>
2019-08-22 22:08:26 +0300

    The third day challenge

---
+++ Day3.js
@@ -0,0 +1,9 @@
+// The third day lesson - Variable Declaration 
+// Saliha Almalki
+
+var day = 3;
+var lesson = " Literals and Variable Declaration "
+var variable = " use it to storing data values "
+document.write(day); // print 3
+document.write(lesson); // print Literals and Variable Declaration
+document.write(variable); // print use to storing data values
