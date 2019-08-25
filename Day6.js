79726f705df5b13a0a1cf43405d88c7e3a8f6a9d
Saliha Almalki <44327944+Saliha21@users.noreply.github.com>
2019-08-25 23:40:14 +0300

    day 6, Identifiers.

---
+++ Day6.js
@@ -0,0 +1,16 @@
+// The sixth day lesson - Identifiers 
+// Saliha Almalki
+
+
+/* Identifiers identify variables, labels, Function.
+ Identifiers are not variables. */
+
+var day = 6; // the variable day is identifier
+var lesson = " Identifiers."; 
+function experiment(){  // the function experiment is identifier
+	document.write("day "+ day);
+  document.write(" lesson is" +lesson);
+}
+
+experiment();
+
