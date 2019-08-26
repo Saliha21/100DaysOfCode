51bbcaa7d509657cd18a9bab77d1dc248d187b30
Saliha Almalki <44327944+Saliha21@users.noreply.github.com>
2019-08-26 20:50:19 +0300

    Day 7, Reserved Words.

---
+++ Day7.js
@@ -0,0 +1,6 @@
+// The seventh day lesson - Reserved Words 
+// Saliha Almalki
+var keyWordsExamble=" break , case , catch , continue , debugger , default , delete , do , else , finally , for , function , if ";
+  document.write( keyWordsExamble );
+
+// لا نستطيع استخدام الكلمات المحجوزة كمعرفات
