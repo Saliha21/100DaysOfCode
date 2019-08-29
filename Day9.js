153f760ceba204830757809c2c42e38bddc2a155
Saliha Almalki <44327944+Saliha21@users.noreply.github.com>
2019-08-29 23:41:33 +0300

    Day 9, types & numbers.

---
+++ Day9.js
@@ -0,0 +1,13 @@
+// The ninth day lesson, types & numbers.
+// Saliha Almalki
+/* - primitive types, number, string, boolean, null, undefined.
+   - object types, global object, array, function, Classes, Constructors, garbage collection.
+*/
+var num1 = 5.6;
+var num2 = 4.8;
+var num3 = 8;
+document.write(Math.pow(num1,2));
+document.write("<br>");
+document.write(Math.ceil(num2));
+document.write("<br>");
+document.write(Math.max(num1,num2,num3));
