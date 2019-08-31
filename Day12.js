7e4888b04f2dd4b51658a908b93298c75c1a5314
Saliha Almalki <44327944+Saliha21@users.noreply.github.com>
2019-08-31 22:23:04 +0300

    Day 12, console.

---
+++ Day12.js
@@ -0,0 +1,4 @@
+//The twelfth day lesson - console
+//Saliha Almalki
+/*The Console object provides access to the browser's debugging console*/
+console.log(new Date()); // log() method writes a message to the console.
