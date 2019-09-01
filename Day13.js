ab90c890a9566797c2434ad0bf1866b9cdb061d8
Saliha Almalki <44327944+Saliha21@users.noreply.github.com>
2019-09-01 23:45:08 +0300

    Day 13, String.

---
+++ Day13.js
@@ -0,0 +1,9 @@
+//The thirteenth day lesson - String
+//Saliha Almalki
+var _String = "A JavaScript string is zero or more characters written inside quotes.";
+var a = _String.charAt(5);
+var b = _String.substring(3,9);
+var c = _String.indexOf("h");
+var d = _String.replace("JavaScript" , "JS");
+
+document.write( a + "<br> " + b + "<br> "+ c + "<br> "+ d );
