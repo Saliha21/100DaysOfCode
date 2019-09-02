e84bebdda2f4b35aa585e6dd486fe5a4dd36bb88
Saliha Almalki <44327944+Saliha21@users.noreply.github.com>
2019-09-02 23:13:45 +0300

    Day 14, Operators.

---
+++ Day14.js
@@ -0,0 +1,19 @@
+//The fourteenth day lesson - Operators.  Saliha Almalki
+//----------------Arithmetic Operators---------------
+var a1 = 20*4;  
+var a2 = 20+4;
+var a3 = 20-4;
+var a4 = 20/4;
+document.write( a1 + "<br> " + a2 + "<br>" + a3 +"<br>"+ a4 + "<br>");
+//----------------Comparison Operators--------------
+/*Comparison operator compares the two operands
+The output True or False.*/
+var b1 = 5 != 9; 
+var b2 = 5 === 9;
+var b3 = 5 <= 9;
+document.write( "<br>" + b1 + "<br> " + b2 + "<br>" + b3 +"<br>");
+//----------------Logical Operators-----------------
+var c1 = (10==10 && 20==33);
+var c2 = (10==10 || 20==33);
+var c3 = !(10==10);
+document.write( "<br>" + c1 + "<br> " + c2 + "<br>" + c3 +"<br>");
