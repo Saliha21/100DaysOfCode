c855151017956d6fe63d1e5205e56470fb99e723
Saliha Almalki <44327944+Saliha21@users.noreply.github.com>
2019-09-03 23:38:07 +0300

    Day 15, Conditional Statements.

---
+++ Day15.js
@@ -0,0 +1,21 @@
+//The fifteenth day lesson - Conditional Statements.  Saliha Almalki
+/* The syntax 
+if (condition) {
+block of code to be executed if the condition is true
+} */
+
+// if statement.
+var degree=50;
+if(degree>=50){
+  document.write("good");
+}
+// else statement.
+var degree=40;
+if(degree>=50){
+  document.write("good");
+}
+else{
+  document.write("<br>"+ "bad");
+}
+
+
