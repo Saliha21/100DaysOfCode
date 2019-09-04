cc9c42de8b9df3d7f9dc06d3cdd68bc01e231971
Saliha Almalki <44327944+Saliha21@users.noreply.github.com>
2019-09-04 23:39:41 +0300

    Day 16, Conditional Statements - else if.

---
+++ Day16.js
@@ -0,0 +1,23 @@
+//The sixteenth day lesson - Conditional Statements. Saliha Almalki
+
+// else if statement.
+
+var degree=30;
+if(degree>=90){
+  document.write("Your result:"+" A ");
+}
+else if(degree>=80){
+  document.write("Your result:"+ " B ");
+}
+else if(degree>=70){
+  document.write("Your result:"+ " C ");
+}
+else if(degree>=60){
+  document.write("Your result:"+ " D ");
+}
+else{
+  document.write("Your result:"+ " F ");
+}
+
+
+
