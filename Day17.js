4f3d680daad7fc8a66e12774934133026c349b39
Saliha Almalki <44327944+Saliha21@users.noreply.github.com>
2019-09-07 23:40:27 +0300

    Day 17, Switch statement.

---
+++ Day17.js
@@ -0,0 +1,14 @@
+//The seventeenth day lesson - Switch statement. 
+//Saliha Almalki
+var lamp = 0;
+switch(lamp) {
+case 0:
+ document.write(" Close ");
+ break;
+case 1:
+ document.write(" Open ");
+ break;
+default:
+ document.write(" No value ");
+ break;
+}
