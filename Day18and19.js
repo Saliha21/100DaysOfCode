e3bb7ddd35d5e96c6e9069eec93583068889306a
Saliha Almalki <44327944+Saliha21@users.noreply.github.com>
2019-09-07 23:43:38 +0300

    Day 18-19, Convert switch to if statement.

---
+++ Day18and19.js
@@ -0,0 +1,12 @@
+//The eighteenth anf nineteenth day lesson - Convert switch statement to if statement. 
+//Saliha Almalki
+var browser = "Edge"
+if (browser === "Edge") {
+ alert( "You've got the Edge!" );
+}
+else if (browser === "Chrome" || browser === "Firefox" || browser === "Safari" || browser === "Opera"){
+   alert( 'Okay we support these browsers too' );
+}
+ else{
+ alert( 'We hope that this page looks ok!' );
+}
