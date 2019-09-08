e07f758d0152804963012cf3a67ec5ddcdaaf602
Saliha Almalki <44327944+Saliha21@users.noreply.github.com>
2019-09-08 23:53:44 +0300

    Day 20, loops - while.

---
+++ Day20.js
@@ -0,0 +1,7 @@
+// 20-100 * loops - while *
+//Saliha Almalki
+var number = 20;
+while (number <= 100) { // "While" is used to repeat a specific block, until a condition is false.
+ document.write(number+"<br>");
+  number+= 10; // increment by 10.
+}
