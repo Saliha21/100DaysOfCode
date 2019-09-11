0ec2789ec0531f30c3645e1187069758697ea53e
Saliha Almalki <44327944+Saliha21@users.noreply.github.com>
2019-09-11 10:28:41 +0300

    Day 21, do/while.

---
+++ day21.js
@@ -0,0 +1,10 @@
+// 21-100 * loops - do/while *
+//Saliha Almalki
+
+var number = 6;
+do {
+  document.write(number +"<br>");
+   number+= 1;// increment by 10.
+}
+while (number <= 9);
+ document.write(number+"<br>");
