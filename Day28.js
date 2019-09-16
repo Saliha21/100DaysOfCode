21a7dd4983ac4a85671dbe39f0d15169b8d48ca4
Saliha Almalki <44327944+Saliha21@users.noreply.github.com>
2019-09-16 18:07:52 +0300

    Day 28, Jumps - throw.

--- Day28.js
+++ Day28.js
@@ -10,11 +10,12 @@ function division(num1, num2){
 division(4,0); // output: num2 must not be zero
 
 /*
-function sum(num1, num2){
-  if(num2==0) throw "num2 must not be zero";
+function division(num1, num2){
+  if(num2==0) throw "num2 must not be zero"; 
+  // throw statement allows you to create a error messages.
   else 
     return "num1 / num2 = " + ( num1 / num2 );
 }
-sum(4,8);  */
+division(4,8);  */
  
  // output: num1 / num2 = 0.5
