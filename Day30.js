238176ad9c93ff6f9bfaab671143e03fb10f9a0f
Saliha Almalki <44327944+Saliha21@users.noreply.github.com>
2019-09-21 15:23:24 +0300

    Day 30, Object.

---
+++ Day30.js
@@ -0,0 +1,10 @@
+// 30-100 * Object *
+// Saliha Almalki
+var clothes={type:"pant", 
+             size:"30", 
+             color:"red" , 
+             brand:"Lacoste"};// define the object
+document.write(typeof clothes+"<br>"); 
+document.write(clothes["type"]);
+document.write("<br>"+clothes["size"]);
+document.write("<br>"+clothes.color);
