99a1a4bb4c5df8075759890620db994b3eb42225
Saliha Almalki <44327944+Saliha21@users.noreply.github.com>
2019-09-27 23:35:26 +0300

    Day 37, Testing Properties.

---
+++ Day37.js
@@ -0,0 +1,9 @@
+//37-100 * Testing Properties *
+//Saliha Almalki
+var car = {
+  name: "BMW", 
+  color: "White",
+  price: 300 }
+document.write("price" in car );
+document.write("<br>");
+document.write(car.hasOwnProperty("name"));
