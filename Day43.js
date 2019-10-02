2f852da2d8bb56bc13d8f26cd4e1e1530597c6da
Saliha Almalki <44327944+Saliha21@users.noreply.github.com>
2019-10-02 23:11:54 +0300

    Day 43, Array.

---
+++ Day43.js
@@ -0,0 +1,11 @@
+//43-100 * Array *
+// Saliha Almalki
+var fruit = ["Orange","Banana","Apple","Cherry"]; // declaration array
+var number = [4, 6,8];
+//OR
+var habits = new Array("Reading","Writing","Drowing");
+// this way is not a good way for simplicity, readability and execution speed.
+document.write(habits);
+document.write("<br>"+fruit);
+
+
