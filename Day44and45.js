02944560fe749733eadfad2b7db9b5d3d732f6b1
Saliha Almalki <44327944+Saliha21@users.noreply.github.com>
2019-10-03 23:44:14 +0300

    Day 44-45, Reading and Accessing elements.

---
+++ Day44and45.js
@@ -0,0 +1,11 @@
+// 44 and 45-100 * Reading and Accessing elements *
+// Saliha Almalki
+ 
+var toDoList = ["Drink more water", "Sleep early", "Read 15 bage"];
+document.write(toDoList[0]+ "<br>");
+var task1 = toDoList[2];
+document.write(task1+"<br>");
+for(var i=0; i <=toDoList.length; i++){
+  document.write(toDoList[i]+" - ");
+}
+  
