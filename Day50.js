6d7e9d42088be82793d32eaa1aac4d199f10c9fc
Saliha Almalki <44327944+Saliha21@users.noreply.github.com>
2019-10-19 21:33:40 +0300

    Deleting array elements.

---
+++ Day50.js
@@ -0,0 +1,8 @@
+// 45-100 * deleteing array elements *
+var arr = ["sara","yara","seren"];
+document.write(arr);
+document.write("<br>" + arr.length);
+delete arr[1];
+document.write("<br>" + arr);
+document.write("<br>" + arr.length);
+document.write("<br>" + arr[1]);
