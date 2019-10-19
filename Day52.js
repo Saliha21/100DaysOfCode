c2fa2588ad0106e1b3d7ea4e9d3d1f5ef343c290
Saliha Almalki <44327944+Saliha21@users.noreply.github.com>
2019-10-19 21:37:59 +0300

    Multidimensional Array.

---
+++ Day52.js
@@ -0,0 +1,9 @@
+// 52-100 * Multidimensional Array *
+var activities = [['Working', 7], 
+                  ['Eating', 6], 
+                  ['Walking', 4], 
+                  ['Playing Game', 8], 
+                  ['Sleeping', 9]];
+
+for (var i = 0 ; i< activities.length ;i++){
+document.write(activities[i] + "<br>");}
