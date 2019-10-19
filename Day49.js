0d24c535eb86a2ee9436d8a31d7b7ab5d04cb80d
Saliha Almalki <44327944+Saliha21@users.noreply.github.com>
2019-10-19 21:20:04 +0300

    Adding array elements.

---
+++ Day49.js
@@ -0,0 +1,9 @@
+// 49-100 * adding array elements *
+var arr = [];
+arr[0] = "sara";
+arr[1] = "yara";
+document.write(arr);
+//or
+arr.push("seren");
+arr.push("Ahmad");
+document.write("<br>"+arr);
