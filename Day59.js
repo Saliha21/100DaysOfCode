42b46ce9d109be462ab960e20e5d79256f6d8d08
Saliha Almalki <44327944+Saliha21@users.noreply.github.com>
2019-10-25 00:50:53 +0300

    Strings As Arrays.

---
+++ Day59.js
@@ -0,0 +1,18 @@
+// 59 - 100 * Array methods *
+var str = "HELLO WORLD";
+var res = str.charAt(0); // 
+var resEnd = str.charAt(str.length-1);// 
+document.write(res);
+document.write("<br>");
+document.write(resEnd);
+
+var str3 = "Hello world, welcome to the universe.";
+var n = str3.includes("world");
+document.write("<br>");
+document.write(n);
+
+var str1 = "ab";
+var str2 = "cd";
+var n = str1.localeCompare(str2);
+document.write("<br>");
+document.write(n);
