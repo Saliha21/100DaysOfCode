5bb11ce56175b7460bc1ab476f0c5901e1dc966a
Saliha Almalki <44327944+Saliha21@users.noreply.github.com>
2019-10-25 00:41:56 +0300

    Array methods.

---
+++ Day56.js
@@ -0,0 +1,11 @@
+// 56 - 100 * Array methods *
+var fruits = ["Banana", "Orange", "Apple", "Mango"] ;
+ document.write(fruits.splice(1, 2 ,"Lemon", "Kiwi")) ;
+ fruits.push("Kiwi"); // add element at the end of an array.
+ document.write(fruits);
+ fruits.pop(); // remove the last element.
+ document.write(fruits);
+ fruits.unshift("Lemon", "Pineapple"); // add element at the beginning
+ document.write(fruits);
+ fruits.shift(); // remove the first element.
+ document.write(fruits);
