19b58fa1bc5bdb3e5d4f6f55b818a275f7d27097
Saliha Almalki <44327944+Saliha21@users.noreply.github.com>
2019-10-29 21:36:44 +0300

    Function

---
+++ Day62.js
@@ -0,0 +1,11 @@
+// 62 - 100 * function *
+
+function printprops(o) {
+	for (var p=0; p<o;p++){
+		document.write("p = " + p +"<br>");}}
+printprops(5);
+
+function multi(x,y) { 
+  var z = x*y;
+		return "x*y = " +  z ;}
+document.write(multi(5,6));
