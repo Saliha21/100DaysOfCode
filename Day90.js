d17b7842bf47d8ed68ec9822a39d9ebdf41308f9
Saliha Almalki <44327944+Saliha21@users.noreply.github.com>
2019-11-26 02:45:35 +0300

    Week 14

--- Day90.js
+++ Day90.js
@@ -1,10 +1,6 @@
 // 90-100
-var url_string = "http://www.example.com/t.html?a=1&b=3&username=m2-m3-m4-m5";
-var url = new URL(url_string); 
-var c = url.searchParams.get("a"); 
-document.write(c); 
-//----------------------------------
-var params = (new URL("https://example.com/?name=Jonathan%20Smith&age=18")).searchParams; 
-var name = params.get("name");
-var age = parseInt(params.get("age"));
-document.write(age); 
+var uRl = prompt("Please enter your url");
+var url = new URL(uRl); 
+var val = prompt("Please enter your value");
+var c = url.searchParams.get(val); 
+document.write(c);
